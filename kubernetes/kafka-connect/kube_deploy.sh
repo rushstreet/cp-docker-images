1fe5b6542a5cf8e6ae02a10f01487b705835dd5b
Tynan Douglas <tynan.douglas@gmail.com>
2018-07-20 14:47:16 -0500

    copy files

--- kubernetes/kafka-connect/kube_deploy.sh
+++ kubernetes/kafka-connect/kube_deploy.sh
@@ -1,2 +1,2 @@
 pwd > /home/ubuntu/dir.txt
-kubectl apply -f kubernetes/kafka-connect/deployment.yml > /home/ubuntu/hello.txt
+#kubectl apply -f kubernetes/kafka-connect/deployment.yml > /home/ubuntu/hello.txt
