b9b4ecee09897eea1d8e303c46689fb79e025b28
Tynan Douglas <tynan.douglas@gmail.com>
2018-07-23 14:46:41 -0400

    set kube config

--- kubernetes/kafka-connect/kube_deploy.sh
+++ kubernetes/kafka-connect/kube_deploy.sh
@@ -1,1 +1,5 @@
+kubectl config set-cluster "$KUBE_CLUSTER_NAME" --server="$KUBE_SERVER" --insecure-skip-tls-verify="true"
+kubectl config set-credentials "$KUBE_CLUSTER_NAME" --token="$KUBE_TOKEN"
+kubectl config set-context "$KUBE_CLUSTER_NAME" --cluster"$KUBE_CLUSTER_NAME" --user="$KUBE_CLUSTER_NAME"
+kubectl config use-context "$KUBE_CLUSTER_NAME"
 kubectl apply -f /tmp/deployment.yml
