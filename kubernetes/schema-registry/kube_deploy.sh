83ed1f510e176fdab9dda2149312f3b83b4e8074
Tynan Douglas <tynan.douglas@gmail.com>
2018-07-26 10:56:37 -0400

    schema-registry temp subdir

--- kubernetes/schema-registry/kube_deploy.sh
+++ kubernetes/schema-registry/kube_deploy.sh
@@ -1,3 +1,3 @@
-kubectl apply -f /tmp/deployment.yml
-kubectl apply -f /tmp/service.yml
-kubectl apply -f /tmp/ingress.yml
+kubectl apply -f /tmp/schema-registry/deployment.yml
+kubectl apply -f /tmp/schema-registry/service.yml
+kubectl apply -f /tmp/schema-registry/ingress.yml
