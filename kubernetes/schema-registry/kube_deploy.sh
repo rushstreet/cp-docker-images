2cac8bb146a18a509b3ed09491ef0af052fae8fd
Tynan Douglas <tynan.douglas@gmail.com>
2018-07-26 12:19:35 -0400

    remove schema registry ingress

--- kubernetes/schema-registry/kube_deploy.sh
+++ kubernetes/schema-registry/kube_deploy.sh
@@ -1,3 +1,3 @@
 kubectl apply -f /tmp/schema-registry/deployment.yml
 kubectl apply -f /tmp/schema-registry/service.yml
-kubectl apply -f /tmp/schema-registry/ingress.yml
+#kubectl apply -f /tmp/schema-registry/ingress.yml
